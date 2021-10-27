c19272233e0fe03f77646f58d3d362af4d62652f
hotbagels <81516937+hotbagels@users.noreply.github.com>
2021-10-27 17:31:56 -0400

    query parameters added for initial store categories

--- app.js
+++ app.js
@@ -1,35 +1,40 @@
-require("dotenv").config();
-// async errors
+require('dotenv').config();
+require('express-async-errors')
 
-const express = require("express");
+const express = require('express');
 const app = express();
 
-const notFoundMiddleware = require("./middleware/not-found");
-const errorMiddleware = require("./middleware/error-handler");
+const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
 
 // middleware
 app.use(express.json());
 
 // routes
-app.get("/", (req, res) => {
+
+app.get('/', (req, res) => {
   res.send(`<h1>Store API</h1><a href="/api/v1/products">Product Route</a>`);
 });
 
+app.use('/api/v1/products',productsRouter);
 // product route
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
 
-const port = process.env.PORT|| 3000
-
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
     // connect DB
-    app.listen(port, console.log(`Server listening on port ${port}...`))
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server listening on port ${port}...`));
   } catch (error) {
-      console.log(error);
+    console.log(error);
   }
 };
 
-start()
+start();
