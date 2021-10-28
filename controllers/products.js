0aee0e27b3b82313687b88a69c799af656646191
hotbagels <81516937+hotbagels@users.noreply.github.com>
2021-10-28 09:37:57 -0400

    added numeric filtering to product query for price and rating

--- controllers/products.js
+++ controllers/products.js
@@ -35,10 +35,13 @@ const getAllProducts = async (req, res) => {
       regEx,
       (match) => `-${operatorMap[match]}-`
     );
-    const options = ['price', rating]
-    filters = filters.split(',').forEach((item)=> {
-     const [] = item.split('-')
-    })
+    const options = ['price', 'rating'];
+    filters = filters.split(',').forEach((item) => {
+      const [field, operator, value] = item.split('-');
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
   }
   console.log(queryObject);
 
